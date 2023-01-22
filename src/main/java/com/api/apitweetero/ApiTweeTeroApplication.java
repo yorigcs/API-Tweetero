109276ab6408233f3e24fcd6b347d9135d421431
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-22 18:07:11 -0300

    chore: init spring boot project

---
+++ src/main/java/com/api/apitweetero/ApiTweeTeroApplication.java
@@ -0,0 +1,13 @@
+package com.api.apitweetero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTweeTeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTweeTeroApplication.class, args);
+    }
+
+}
