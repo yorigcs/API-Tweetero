0f7ad3ee82fb118594e1f8657988a26909c3165a
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-23 00:39:10 -0300

    feat: add findByUsername query to UsersRepository

--- src/main/java/com/api/apitweetero/repositories/UsersRepository.java
+++ src/main/java/com/api/apitweetero/repositories/UsersRepository.java
@@ -4,4 +4,6 @@ import com.api.apitweetero.models.UsersModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<UsersModel,String> {
+    UsersModel findByUsername(String name);
+
 }
