95438d7059cfca662069f5af568fa47eb3f35a3e
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-23 02:05:46 -0300

    feat: create findByUsername TweetsRepository query

--- src/main/java/com/api/apitweetero/repositories/TweetsRepository.java
+++ src/main/java/com/api/apitweetero/repositories/TweetsRepository.java
@@ -3,5 +3,8 @@ package com.api.apitweetero.repositories;
 import com.api.apitweetero.models.TweetsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TweetsRepository extends JpaRepository<TweetsModel,String> {
+    List<TweetsModel> findByUsername(String username);
 }
